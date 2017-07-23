54d6cffb265bbe11c800c093df938b0f53c5660b
Thomas Adjadj <ta@ecofip.com>
2017-07-23 22:10:39 +0200

    master

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ var api = new ParseServer({
   databaseURI: databaseUri || 'mongodb://root:tomadj@ds034797.mlab.com:34797/parse',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
   appId: process.env.APP_ID || 'myAppId',
-  masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
+  masterKey: process.env.MASTER_KEY || 'myMasterKey', //Add your master key here. Keep it secret!
   serverURL: process.env.SERVER_URL || 'https://tomadj.herokuapp.com/parse',  // Don't forget to change to https if needed
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
